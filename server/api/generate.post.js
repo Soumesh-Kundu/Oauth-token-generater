7faa7dfbb0c47a0f777883fae8bd49a186075476
Soumesh Kundu <iamsoumo26@gmail.com>
2023-08-16 18:09:32 +0530

    completed

--- server/api/generate.post.js
+++ server/api/generate.post.js
@@ -1,9 +1,8 @@
 import google from '@googleapis/gmail'
 
 export default defineEventHandler(async (event)=>{
-    const {origin}=getRequestURL(event)
     const {clientId,clientSecret}=await readBody(event)
-    const OAuth2Client = new google.auth.OAuth2(clientId, clientSecret, `${origin}/callback`)
+    const OAuth2Client = new google.auth.OAuth2(clientId, clientSecret,  `${useRuntimeConfig().origin}/callback`)
     const url = OAuth2Client.generateAuthUrl({
         access_type: 'offline',
         prompt: 'consent',
