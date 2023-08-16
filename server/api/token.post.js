e4901356b3141814af9936dca92b9f00d766ff8a
Soumesh <107293904+Soumesh-Kundu@users.noreply.github.com>
2023-08-16 18:22:28 +0530

    Update token.post.js

--- server/api/token.post.js
+++ server/api/token.post.js
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event)=>{
     try {
         const {code}=getQuery(event)
         const {clientId,clientSecret}=await readBody(event)
-        const OAuth2Client = new google.auth.OAuth2(clientId, clientSecret, `https://oauth-token-generater.vercel.app/callback`)
+        const OAuth2Client = new google.auth.OAuth2(clientId, clientSecret, 'https://oauth-token-generater.vercel.app/callback')
         const {tokens}=await OAuth2Client.getToken(code);
         return tokens
     } catch (error) {
